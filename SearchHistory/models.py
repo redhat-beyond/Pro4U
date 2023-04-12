ee432146084c07f644025c1ea1b3e018253d9b26
Ofir Bahcar <109077726+OfirBachar@users.noreply.github.com>
2023-04-12 12:24:43 +0300

    Update models.py

--- SearchHistory/models.py
+++ SearchHistory/models.py
@@ -1,4 +1,3 @@
-import uuid
 from django.db import models
 from django.utils import timezone
 # from client.models import Client
@@ -6,7 +5,7 @@ from django.utils import timezone
 
 
 class SearchHistory(models.Model):
-SearchID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-# ProfessionalID = models.ForeignKey(Professional, on_delete=models.CASCADE)
-# ClientID = models.ForeignKey(Client, on_delete=models.CASCADE)
-Date = models.DateTimeField(default=timezone.now)
+    SearchID = models.BigAutoField(primary_key=True)
+    # ProfessionalID = models.ForeignKey(Professional, on_delete=models.CASCADE)
+    # ClientID = models.ForeignKey(Client, on_delete=models.CASCADE)
+    Date = models.DateTimeField(default=timezone.now)
