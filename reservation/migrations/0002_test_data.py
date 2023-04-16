85aefa190e9f8da8b90753039e34856a433bb89c
Ido Singer <idosinger1998@gmail.com>
2023-04-16 16:21:32 +0300

    Update reservation app
    I deleted the file views.py.
    I deleted the file tests.py.
    Adding test data.
    Add data model functions for the more complex data operations.

---
+++ reservation/migrations/0002_test_data.py
@@ -0,0 +1,68 @@
+from django.db import migrations, transaction
+from datetime import datetime
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('reservation', '0001_initial'),
+    ]
+
+    def generate_data(apps, schema_editor):
+        from reservation.models import Schedule
+        test_data = [
+            (1, 1, datetime(2023, 4, 16, 14, 00, 0), datetime(2023, 4, 16, 19, 00, 0), 60),
+            (2, 1, datetime(2023, 4, 19, 14, 00, 0), datetime(2023, 4, 19, 20, 00, 0), 60),
+            (3, 2, datetime(2023, 4, 16, 14, 00, 0), datetime(2023, 4, 16, 19, 00, 0), 60),
+            (4, 2, datetime(2023, 4, 19, 14, 00, 0), datetime(2023, 4, 19, 20, 00, 0), 60),
+        ]
+        with transaction.atomic():
+            for schedule_id, professional_id, start_day, end_day, meeting_time in test_data:
+                Schedule(
+                    schedule_id=schedule_id,
+                    professional_id=professional_id,
+                    start_day=start_day,
+                    end_day=end_day,
+                    meeting_time=meeting_time
+                ).save()
+
+    def generate_data2(apps, schema_editor):
+        from reservation.models import TypeOfJob
+        test_data = [
+            (1, 1, "man haircut", 70),
+            (2, 1, "woman haircut", 100),
+            (3, 2, "Gel nail polish", 80),
+        ]
+        with transaction.atomic():
+            for typeOfJob_id, professional_id, typeOfJob_name, price in test_data:
+                TypeOfJob(
+                    typeOfJob_id=typeOfJob_id,
+                    professional_id=professional_id,
+                    typeOfJob_name=typeOfJob_name,
+                    price=price
+                ).save()           
+
+    def generate_data3(apps, schema_editor):
+        from reservation.models import Appointment, TypeOfJob
+        test_data = [
+            (1, 1, 1, 1, datetime(2023, 4, 16, 15, 00, 0), datetime(2023, 4, 16, 16, 00, 0), ""),
+            (2, 1, 2, 2, datetime(2023, 4, 16, 16, 00, 0), datetime(2023, 4, 16, 17, 00, 0), ""),
+            (3, 2, 1, 1, datetime(2023, 4, 16, 18, 00, 0), datetime(2023, 4, 16, 19, 00, 0), ""),
+        ]
+        with transaction.atomic():
+            for appointment_id, professional_id, client_id, typeOfJob_id, start_appointment, end_appointment, summary \
+                                                                                                        in test_data:
+                Appointment(
+                    appointment_id=appointment_id,
+                    professional_id=professional_id,
+                    client_id=client_id,
+                    typeOfJob_id=TypeOfJob.objects.get(pk=typeOfJob_id),
+                    start_appointment=start_appointment,
+                    end_appointment=end_appointment,
+                    summary=summary
+                ).save()
+                
+    operations = [
+        migrations.RunPython(generate_data),
+        migrations.RunPython(generate_data2),
+        migrations.RunPython(generate_data3),
+    ]
--- reservation/models.py
+++ reservation/models.py
@@ -1,39 +1,99 @@
 from django.db import models
+from datetime import date, datetime, timedelta
 # from professional.models import Professional
 # from client.models import Client
 
 
 class TypeOfJob(models.Model):
-    typeOfJob_ID = models.BigAutoField(primary_key=True)
-    # professional_ID = models.ForeignKey(Professional, on_delete=models.CASCADE)
-    typeOfName = models.CharField(max_length=120)
+    typeOfJob_id = models.BigAutoField(primary_key=True)
+    # professional_id = models.ForeignKey(Professional, on_delete=models.CASCADE)
+    professional_id = models.PositiveIntegerField(null=False, blank=False)
+    typeOfJob_name = models.CharField(max_length=120)
     price = models.PositiveIntegerField(null=False, blank=False)
 
     def __str__(self):
-        return self.typeOfName
+        return self.typeOfJob_name
 
     class Meta:
         db_table = 'TypeOfJob'
 
+    @staticmethod
+    def get_typeofjobs_name(professional_id: int):
+        typeofjobs_name_list = TypeOfJob.objects.filter(professional_id=professional_id)\
+                                                        .values_list('typeOfJob_name', flat=True)
+        return typeofjobs_name_list
+
 
 class Appointment(models.Model):
-    appointment_ID = models.BigAutoField(primary_key=True)
-    # professional_ID = models.ForeignKey(Professional, on_delete=models.CASCADE)
-    # client_ID = models.ForeignKey(Client, on_delete=models.CASCADE)
-    typeOfJob_ID = models.ForeignKey(TypeOfJob, on_delete=models.CASCADE)
+    appointment_id = models.BigAutoField(primary_key=True)
+    # professional_id = models.ForeignKey(Professional, on_delete=models.CASCADE)
+    # client_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    professional_id = models.PositiveIntegerField(null=False, blank=False)
+    client_id = models.PositiveIntegerField(null=False, blank=False)
+    typeOfJob_id = models.ForeignKey(TypeOfJob, on_delete=models.CASCADE)
     start_appointment = models.DateTimeField()
     end_appointment = models.DateTimeField()
-    summary = models.TextField()
+    summary = models.TextField(blank=True)
 
     class Meta:
         db_table = 'Appointment'
 
+    @staticmethod
+    def get_clients(professional_id: int):
+        clients_list = Appointment.objects.filter(professional_id=professional_id).values_list('client_id', flat=True)
+        return clients_list
+
+    @staticmethod
+    def get_client_list_on_certain_day(professional_id: int, date: date):
+        client_list_on_certain_day = Appointment.objects.filter(professional_id=professional_id,
+                                                                start_appointment__year=date.year,
+                                                                start_appointment__month=date.month,
+                                                                start_appointment__day=date.day)\
+                                                                .values_list('client_id', flat=True)
+        return client_list_on_certain_day
+
 
 class Schedule(models.Model):
-    schedule_ID = models.BigAutoField(primary_key=True)
-    # professional_ID = models.ForeignKey(Professional, on_delete=models.CASCADE)
+    schedule_id = models.BigAutoField(primary_key=True)
+    # professional_id = models.ForeignKey(Professional, on_delete=models.CASCADE)
+    professional_id = models.PositiveIntegerField(null=False, blank=False)
     start_day = models.DateTimeField()
     end_day = models.DateTimeField()
+    meeting_time = models.PositiveIntegerField(null=False, blank=False)
+
+    @staticmethod
+    def get_possible_meetings(professional_id: int, date: date):
+        professional_schedule_day = Schedule.objects.filter(professional_id=professional_id,
+                                                            start_day__year=date.year,
+                                                            start_day__month=date.month,
+                                                            start_day__day=date.day)
+        meeting_time = professional_schedule_day[0].meeting_time
+        meetings = []
+        start_time = datetime.combine(date, professional_schedule_day[0].start_day.time())
+        end_time = datetime.combine(date, professional_schedule_day[0].end_day.time())
+        while start_time + timedelta(minutes=meeting_time) <= end_time:
+            meetings_str = f"{start_time.time().strftime('%H:%M')}-" \
+                           f"{(start_time + timedelta(minutes=meeting_time)).time().strftime('%H:%M')}"
+            meetings.append(meetings_str)
+            start_time += timedelta(minutes=meeting_time)
+        return meetings
+
+    @staticmethod
+    def get_free_meetings(professional_id: int, date: date):
+        free_meetings = []
+        meetings = Schedule.get_possible_meetings(professional_id, date)
+        for i, j in enumerate(meetings):
+            start_meetings = meetings[i].split("-")[0]
+            exists_appointment = Appointment.objects.filter(professional_id=professional_id,
+                                                            start_appointment__year=date.year,
+                                                            start_appointment__month=date.month,
+                                                            start_appointment__day=date.day,
+                                                            start_appointment__hour=int(start_meetings.split(":")[0]),
+                                                            start_appointment__minute=int(start_meetings.split(":")[1]))
+            if len(exists_appointment) == 0:
+                free_meetings.append(meetings[i])
+
+        return free_meetings
 
     class Meta:
         db_table = 'Schedule'
