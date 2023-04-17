7e4477b78e4cc929837d850e5ab30e7c9f885263
Ido Yekutiel <idoyekutiel123@gmail.com>
2023-04-18 00:20:53 +0300

    Fixing models.py, adding text_data.py

--- proImages/models.py
+++ proImages/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Images(models.Model):
-    images_ID = models.BigAutoField(primary_key=True)
-    # professional_ID = models.ForeignKey(Professional, on_delete=models.CASCADE)
+    image_id = models.BigAutoField(primary_key=True)
+    professional_id = models.PositiveIntegerField()
     image = models.ImageField(null=True, blank=True, upload_to="images/")
     likes = models.PositiveIntegerField()
 
