c51d4bcfa63174469389e19f90918420bb3036c7
patrisiakaplun <patrisiakaplun@h-MacBook-Pro-sl-patrisia.local>
2023-04-18 01:50:41 +0300

    Add tests and update models,migrations chatmessage
    adding test.py
    updating models.py and migrations/0001_initial.py.

--- chatmessage/models.py
+++ chatmessage/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 
 class Chatmessage(models.Model):
-    message_id = models.IntegerField()
+    message_id = models.BigAutoField(primary_key=True)
     professional_id = models.CharField(max_length=512)
     client_id = models.CharField(max_length=512)
     date = models.DateTimeField(default=timezone.now)
@@ -14,3 +14,18 @@ class Chatmessage(models.Model):
 
     def __str__(self):
         return f"From {self.professional_id} to {self.client_id}: {self.message} [{self.date}]"
+    
+    @staticmethod
+    def get_all_professional_contacts(professional_id):
+
+        return list({msg.client_id for msg in Chatmessage.objects.filter(professional_id=professional_id)})
+
+    @staticmethod
+    def get_all_client_contacts(client_id):
+
+        return list({msg.professional_id for msg in Chatmessage.objects.filter(client_id=client_id)})
+
+    @staticmethod
+    def get_chat_between_professional_and_client(professional_id, client_id):
+
+        return Chatmessage.objects.filter(professional_id=professional_id, client_id=client_id)
