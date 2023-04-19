01e57d7faf03daeceb1ad7826821e92654ba986f
Ofir Bachar <ofir4bachar@gmail.com>
2023-04-19 13:53:18 +0300

    update models.py ant delete tests.py file
    Signed-off-by: Ofir Bachar <ofir4bachar@gmail.com>

--- SearchHistory/models.py
+++ SearchHistory/models.py
@@ -1,13 +1,28 @@
 from django.db import models
 from django.utils import timezone
-# from client.models import Client
-# from professional.models import Professional
 
 
 class SearchHistory(models.Model):
     search_ID = models.BigAutoField(primary_key=True)
-    # professional_ID = models.ForeignKey(Professional, on_delete=models.CASCADE)
-    # client_ID = models.ForeignKey(Client, on_delete=models.CASCADE)
-    professional_ID = models.CharField(max_length=512)
-    client_ID = models.CharField(max_length=512)
+    professional_ID = models.PositiveIntegerField(null=False, blank=False)
+    client_ID = models.PositiveIntegerField(null=False, blank=False)
     date = models.DateTimeField(default=timezone.now)
+
+
+class Meta:
+    db_table = 'SearchHistory'
+
+
+def __str__(self):
+    return f"{self.client_ID} search {self.professional_ID} [{self.date}]"
+
+
+@staticmethod
+def get_last_professionals_search_by_client(client_ID: int):
+    client_professionals_search = SearchHistory.objects.filter(client_ID=client_ID)
+    return client_professionals_search.get_queryset().order_by('-date')
+
+
+@staticmethod
+def get_clients_that_search_professional(professional_ID: int):
+    return SearchHistory.objects.filter(professional_ID=professional_ID).values_list('client_id', flat=True)
