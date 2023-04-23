93addb82dc01b53f49387abd6734c3392de0c112
patrisiakaplun <patrisiakaplun@h-MacBook-Pro-sl-patrisia.local>
2023-04-23 22:41:01 +0300

    Updating chatmessage model
    Updating chatmessage model after adding client model and profession:
    Updating models.py, 0002_test_data.py and tests.py files
    Adding 0002_chatmessage_sender_type_alter_chatmessage_client_id_and_more.py file

--- chatmessage/tests.py
+++ chatmessage/tests.py
@@ -1,29 +1,32 @@
-from .models import Chatmessage
+from .models import Chatmessage, SenderType
+from account.models.professional import Professional
+from account.models.client import Client
 import pytest
 
-PROFESSIONAL_ID = "professional1"
-CLIENT_ID = "client1"
+
 MESSAGE = "message1"
 
 
 @pytest.fixture
 def chatmessage():
-    return Chatmessage(professional_id=PROFESSIONAL_ID, client_id=CLIENT_ID, message=MESSAGE)
+    return Chatmessage(professional_id=Professional.filter_by_professional_id(1)[0],
+                       client_id=Client.filter_by_client_id(1)[0], message=MESSAGE, sender_type=SenderType.Client)
 
 
 @pytest.fixture
 def persisted_chatmessage(chatmessage):
     chatmessage.save()
     return [(chatmessage.message_id), (chatmessage.professional_id),
-            (chatmessage.client_id), (chatmessage.date), (chatmessage.message)]
+            (chatmessage.client_id), (chatmessage.date), (chatmessage.message), (chatmessage.sender_type)]
 
 
 @pytest.mark.django_db
 class TestChatmessageModel:
     def test_new_member(self, chatmessage):
-        assert chatmessage.professional_id == PROFESSIONAL_ID
-        assert chatmessage.client_id == CLIENT_ID
+        assert chatmessage.professional_id == Professional.filter_by_professional_id(1)[0]
+        assert chatmessage.client_id == Client.filter_by_client_id(1)[0]
         assert chatmessage.message == MESSAGE
+        assert chatmessage.sender_type == SenderType.Client
 
     def test_chatmessage(self, chatmessage):
         chatmessage.save()
@@ -36,12 +39,13 @@ class TestChatmessageModel:
 
     def test_get_professional_contacts(self, persisted_chatmessage):
         assert [persisted_chatmessage[2]] == \
-            Chatmessage.get_all_professional_contacts(professional_id=PROFESSIONAL_ID)
+            [Chatmessage.get_all_professional_contacts(professional_id=Professional.filter_by_professional_id(1)[0])[0]]
 
     def test_get_client_contacts(self, persisted_chatmessage):
         assert [persisted_chatmessage[1]] == \
-          Chatmessage.get_all_client_contacts(client_id=CLIENT_ID)
+            Chatmessage.get_all_client_contacts(client_id=Client.filter_by_client_id(1)[0])
 
     def get_chat_between_professional_and_client(self, persisted_chatmessage):
         assert persisted_chatmessage == \
-            Chatmessage.objects.filter(professional_id=PROFESSIONAL_ID, client_id=CLIENT_ID)
+            Chatmessage.objects.filter(professional_id=Professional.filter_by_professional_id(1)[0],
+                                       client_id=Client.filter_by_client_id(1)[0])
