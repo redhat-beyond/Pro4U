aa4c8ed4175065763062c977b449fa4465fc2506
Guy Beckenstein <guybec@mta.ac.il>
2023-05-07 16:45:44 +0300

    Added conftest.py and created a tests folder
    Signed-off-by: Guy Beckenstein <guybec@mta.ac.il>

--- review/migrations/0002_test_data.py
+++ review/migrations/0002_test_data.py
@@ -1,12 +1,17 @@
 from datetime import timedelta
+
 from django.db import migrations, transaction
 from django.utils import timezone
+
+from account.models.client import Client
+from account.models.professional import Professional
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('review', '0001_initial'),
+        ('account', '0002_test_data'),
     ]
 
     def generate_data(apps, schema_editor):
@@ -14,23 +19,24 @@ class Migration(migrations.Migration):
 
         now = timezone.now()
 
-        test_data = [
-            ('5', 'Excellent!', now - timedelta(days=10), 'user1', 'pro1'),
-            ('4', 'Good', now - timedelta(days=20), 'user2', 'pro2'),
-            ('3', 'Average', now - timedelta(days=5), 'user3', 'pro3'),
-            ('2', 'A simple test review', now - timedelta(days=2), 'user1', 'pro3'),
-            (Review.Rating.FOUR_STARS, 'Another test review', now - timedelta(days=2), 'user3', 'pro2'),
-            (Review.Rating.ONE_STAR, 'Really bad.', now - timedelta(days=2), 'user3', 'pro2'),
+        # Generate review data
+        review_test_data = [
+            ('5', 'Excellent!', now - timedelta(days=10), 1, 1),
+            ('4', 'Good', now - timedelta(days=20), 2, 2),
+            ('3', 'Average', now - timedelta(days=5), 3, 3),
+            ('2', 'A simple test review', now - timedelta(days=2), 1, 3),
+            (Review.Rating.FOUR_STARS, 'Another test review', now - timedelta(days=2), 3, 2),
+            (Review.Rating.ONE_STAR, 'Really bad.', now - timedelta(days=2), 3, 2),
         ]
-
+        # Create review
         with transaction.atomic():
-            for rating, description, date_posted, client_id, professional_id in test_data:
+            for rating, description, date_posted, client_id, professional_id in review_test_data:
                 Review(
                     rating=rating,
                     description=description,
                     date_posted=date_posted,
-                    client_id=client_id,
-                    professional_id=professional_id
+                    client=Client.objects.get(pk=client_id),
+                    professional=Professional.objects.get(pk=professional_id)
                 ).save()
 
     operations = [
--- review/tests/test_review_manager.py
+++ review/tests/test_review_manager.py
@@ -1,54 +1,76 @@
+from typing import Callable
+
 import pytest
-from datetime import timedelta
-from django.utils import timezone
 from review.models import Review
 
-
-@pytest.fixture
-def reviews():
-    now = timezone.now()
-    review1 = Review.objects.create(rating='5', description='Excellent!', date_posted=now - timedelta(days=3),
-                                    client_id='user4', professional_id='pro1')
-    review2 = Review.objects.create(rating='4', description='Good', date_posted=now - timedelta(days=1),
-                                    client_id='user5', professional_id='pro2')
-    review3 = Review.objects.create(rating='3', description='Average', date_posted=now - timedelta(days=2),
-                                    client_id='user6', professional_id='pro3')
-    return [review1, review2, review3]
+TEST_DATA = [
+    (1, '3', 'Creating a test review', 0),
+    (2, '2', 'Creating a test review', 200),
+]
 
 
-@pytest.mark.django_db
-def test_sort_review_by_oldest(reviews):
-    # Test sorting reviews by date (the oldest first)
-    reviews = Review.objects.sort_review_by_oldest()
-    assert_review(reviews.first(), rating='4', client_id='user2', professional_id='pro2')
-    assert_review(reviews.last(), rating='4', client_id='user5', professional_id='pro2')
+def create_reviews(client, client2, professional, create_review: Callable[[str, str, str, str, int], Review]):
+    for client_fixture, rating, description, days in TEST_DATA:
+        if client_fixture == 1:
+            create_review(client, professional, rating, description, days)  # Using factory function
+        else:
+            create_review(client2, professional, rating, description, days)  # Using factory function
 
 
 @pytest.mark.django_db
-def test_sort_review_by_newest(reviews):
-    # Test sorting reviews by date (the newest first)
-    reviews = Review.objects.sort_review_by_newest()
-    assert_review(reviews.first(), rating='4', client_id='user5', professional_id='pro2')
-    assert_review(reviews.last(), rating='4', client_id='user2', professional_id='pro2')
+class TestReviewManager:
+    def test_sort_review_by_oldest(self, client, client2, professional,
+                                   create_review: Callable[[str, str, str, str, int], Review]):
+        create_reviews(client, client2, professional, create_review)
+        # Test sorting sorted_reviews by date (the oldest first)
+        sorted_reviews = Review.objects.sort_review_by_oldest()
+        self.assert_review(sorted_reviews.first(), rating='2',
+                           client_full_name='Bob Builder',
+                           professional_full_name='Bob Builder')
+        self.assert_review(sorted_reviews.last(), rating='3',
+                           client_full_name='Bob Builder',
+                           professional_full_name='Bob Builder')
 
+    def test_sort_review_by_newest(self, client, client2, professional,
+                                   create_review: Callable[[str, str, str, str, int], Review]):
+        create_reviews(client, client2, professional, create_review)
+        # Test sorting sorted_reviews by date (the newest first)
+        sorted_reviews = Review.objects.sort_review_by_newest()
+        self.assert_review(sorted_reviews.first(), rating='3',
+                           client_full_name='Bob Builder',
+                           professional_full_name='Bob Builder')
+        self.assert_review(sorted_reviews.last(), rating='2',
+                           client_full_name='Bob Builder',
+                           professional_full_name='Bob Builder')
 
-@pytest.mark.django_db
-def test_sort_review_by_lowest_rating(reviews):
-    # Test sorting reviews by rating (the lowest first)
-    reviews = Review.objects.sort_review_by_lowest_rating()
-    assert_review(reviews.first(), rating='1', client_id='user3', professional_id='pro2')
-    assert_review(reviews.last(), rating='5', client_id='user1', professional_id='pro1')
+    def test_sort_review_by_lowest_rating(self, client, client2, professional,
+                                          create_review: Callable[[str, str, str, str, int], Review]):
+        create_reviews(client, client2, professional, create_review)
+        # Test sorting sorted_reviews by rating (the lowest first)
+        sorted_reviews = Review.objects.sort_review_by_lowest_rating()
+        self.assert_review(sorted_reviews.first(), rating='1',
+                           client_full_name='Client3 Client3',
+                           professional_full_name='Ido Singer')
+        self.assert_review(sorted_reviews.last(), rating='5',
+                           client_full_name='Client1 Client1',
+                           professional_full_name='Tal Reinfeld')
 
+    def test_sort_review_by_highest_rating(self, client, client2, professional,
+                                           create_review: Callable[[str, str, str, str, int], Review]):
+        create_reviews(client, client2, professional, create_review)
+        # Sort the sorted_reviews by highest rating and verify the first one has the highest rating.
+        sorted_reviews = Review.objects.sort_review_by_highest_rating()
+        self.assert_review(sorted_reviews.first(), rating='5',
+                           client_full_name='Client1 Client1',
+                           professional_full_name='Tal Reinfeld')
+        self.assert_review(sorted_reviews.last(), rating='1',
+                           client_full_name='Client3 Client3',
+                           professional_full_name='Ido Singer')
 
-@pytest.mark.django_db
-def test_sort_review_by_highest_rating(reviews):
-    # Sort the reviews by highest rating and verify the first one has the highest rating.
-    reviews = Review.objects.sort_review_by_highest_rating()
-    assert_review(reviews.first(), rating='5', client_id='user1', professional_id='pro1')
-    assert_review(reviews.last(), rating='1', client_id='user3', professional_id='pro2')
-
-
-def assert_review(review, rating, client_id, professional_id):
-    assert review.rating == rating
-    assert review.client_id == client_id
-    assert review.professional_id == professional_id
+    @staticmethod
+    def assert_review(review, rating, client_full_name, professional_full_name):
+        client_user = review.client.profile_id.user_id
+        professional_user = review.professional.profile_id.user_id
+        assert review.rating == rating
+        assert f'{client_user.first_name} {client_user.last_name}' == client_full_name
+        assert f'{professional_user.first_name} {professional_user.last_name}' == professional_full_name
