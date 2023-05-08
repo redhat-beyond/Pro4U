3e6cacfbf591bd7a76404575d2be9d6ccf5711e8
Ofir Bachar <ofir4bachar@gmail.com>
2023-05-08 15:30:28 +0300

    update SearchHistory model
    Signed-off-by: Ofir Bachar <ofir4bachar@gmail.com>

--- SearchHistory/models.py
+++ SearchHistory/models.py
@@ -1,22 +1,26 @@
 from django.db import models
 from django.utils import timezone
+from account.models.professional import Professional
+from account.models.client import Client
+from django.db.models import Max
 
 
 class SearchHistory(models.Model):
     search_id = models.BigAutoField(primary_key=True)
-    professional_id = models.PositiveIntegerField(null=False, blank=False)
-    client_id = models.PositiveIntegerField(null=False, blank=False)
+    professional_id = models.ForeignKey(Professional, on_delete=models.CASCADE)
+    client_id = models.ForeignKey(Client, on_delete=models.CASCADE)
     date = models.DateTimeField(default=timezone.now)
 
     class Meta:
         db_table = 'SearchHistory'
 
-    @staticmethod
-    def get_5_last_professionals_search_by_client(client_id: int):
-        lst = SearchHistory.objects.filter(client_id=client_id)
-        lst = lst.order_by('-date')[:5]
-        lst = lst.values_list('professional_id', flat=True)
-        return lst
-
     def __str__(self):
         return f"{self.client_id} search {self.professional_id} [{self.date}]"
+
+    @staticmethod
+    def get_last_professionals_search_by_client(client_id, expected_result=5):
+        lst = SearchHistory.objects.filter(client_id=client_id)
+        lst = lst.values('professional_id').annotate(max_date=Max('date'))
+        lst = lst.order_by('-max_date')[:expected_result]
+        lst = [row['professional_id'] for row in lst]
+        return list(lst)
