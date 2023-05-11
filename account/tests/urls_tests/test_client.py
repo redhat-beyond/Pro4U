48cd5b3cb65c75097e24eef190b521e3ace10412
TalReinfeld <talreinfeld1@gmail.com>
2023-05-11 10:38:22 +0300

    Fixing flake8
    Signed-off-by: TalReinfeld <talreinfeld1@gmail.com>

--- account/tests/urls_tests/test_client.py
+++ account/tests/urls_tests/test_client.py
@@ -1,11 +1,9 @@
-from account.models.professional import Professional
-from conftest import professional
-# from django.test import Client
-from fixtures.test import Client
 import pytest
 
 
 BASE_URL = '/client'
+
+
 @pytest.mark.django_db
 class TestFilmViews:
     def test_get_client_profile(self, client, demo_client):
