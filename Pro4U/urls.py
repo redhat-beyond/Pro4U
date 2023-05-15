f97594a045d65fd09328afba50e55c438c9a6f76
patrisiakaplun <patrisiakaplun@h-MacBook-Pro-sl-patrisia.local>
2023-05-16 01:33:17 +0300

    Adding chatmessage template

--- Pro4U/urls.py
+++ Pro4U/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from landing import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.indexView, name='invokeIndexPage')
+    path('', views.indexView, name='invokeIndexPage'),
+    path('', include('reservation.urls')),
+    path('', include('chatmessage.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
