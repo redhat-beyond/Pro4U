3171bafaadd915682f37f35c947892fbf8beebf6
Guy Beckenstein <guybec@mta.ac.il>
2023-05-17 09:29:17 +0300

    Resolved Yariv's Issues
    Signed-off-by: Guy Beckenstein <guybec@mta.ac.il>

--- landing/tests/test_views_and_templates.py
+++ landing/tests/test_views_and_templates.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.urls import resolve
 from pytest_django.asserts import assertTemplateUsed
 
+from landing.models import TeamMember
 from landing.views import learn_more
 
 LEARN_MORE_VIEW_NAME = 'learn_more'
@@ -16,6 +17,14 @@ class TestUrls:
         # Checks that URL endpoint is using view function `learn_more`
         assert resolve(get_url(LEARN_MORE_VIEW_NAME)).func == learn_more
         assert get_response(LEARN_MORE_VIEW_NAME).status_code == 200
+
+    def test_all_team_members_in_context(self, get_url, get_response: Callable[[str], HttpResponse]):
+        # Asserts team members returned in context
+        context_team_members = get_response(LEARN_MORE_VIEW_NAME).context['team_members']
+        expected_team_members = TeamMember.objects.all()
+        assert len(context_team_members) == len(expected_team_members)
+        for context_team_member, expected_team_member in zip(context_team_members, expected_team_members):
+            assert context_team_member == expected_team_member
 
     def test_learn_more_GET_success(self, client, get_response: Callable[[str], HttpResponse]):
         assertTemplateUsed(get_response(LEARN_MORE_VIEW_NAME), 'landing/learn-more.html')
