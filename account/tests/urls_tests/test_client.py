b8e0b4bc169e4f053606a573ce8b189de0b0ea12
TalReinfeld <talreinfeld1@gmail.com>
2023-05-17 10:23:02 +0300

    Fixing test UI throug client
    Switch the "demo_client"/"professional" instznces to the fixtures
    "make_client"/"make_professional".
    
    Call the HTTP reauests from inside the tests instead of
    different method.
    
    Delete the url_tests_test_profile.py for not in use anymore.
    
    Remove the sign up button from the profile.html.
    
    Signed-off-by: TalReinfeld <talreinfeld1@gmail.com>

--- account/tests/urls_tests/test_client.py
+++ account/tests/urls_tests/test_client.py
@@ -1,24 +1,24 @@
 import pytest
-from .test_profile import TestProfileViews, FAKE_ID
 
 
 BASE_URL = '/client'
+FAKE_ID = 10000000
 
 
 @pytest.mark.django_db
-class TestClientViews:
-    def test_get_client_profile(self, client, demo_client):
-        test_client = demo_client
-        response = TestProfileViews.get_profile(client=client, entity_id=test_client.client_id, url=BASE_URL)
+class TestClientProfile:
+    def test_get_client_profile(self, client, make_client):
+        test_client = make_client()
+        response = client.get(f"{BASE_URL}/profile/{test_client.client_id}/")
         assert response.status_code == 200
         assert 'account/profile.html' in [template.name for template in response.templates]
         assert f"{test_client.profile_id.user_id.first_name}'s Profile" in response.content.decode('utf-8')
 
-    def test_show_profile_non_existent_client(self, client, demo_client):
-        response = TestProfileViews.get_profile(client=client, entity_id=FAKE_ID, url=BASE_URL)
+    def test_show_profile_non_existent_client(self, client):
+        response = client.get(f"{BASE_URL}/profile/{FAKE_ID}/")
         assert response.status_code == 404
 
-    def test_show_profile_static_files(self, client, demo_client):
-        test_client = demo_client
-        response = TestProfileViews.get_profile(client=client, entity_id=test_client.client_id, url=BASE_URL)
+    def test_show_profile_static_files(self, client, make_client):
+        test_client = make_client()
+        response = client.get(f"{BASE_URL}/profile/{test_client.client_id}/")
         assert 'img/blank_profile.png' in response.content.decode('utf-8')
--- account/tests/urls_tests/test_professional.py
+++ account/tests/urls_tests/test_professional.py
@@ -1,26 +1,24 @@
 import pytest
-from .test_profile import TestProfileViews, FAKE_ID
 
 
 BASE_URL = '/professional'
+FAKE_ID = 222222222
 
 
 @pytest.mark.django_db
-class TestClientViews:
-    def test_get_client_profile(self, client, professional):
-        test_professional = professional
-        response = TestProfileViews.get_profile(client=client, entity_id=test_professional.professional_id,
-                                                url=BASE_URL)
+class TestProfessionalProfile:
+    def test_get_client_profile(self, client, make_professional):
+        test_professional = make_professional()
+        response = client.get(f"{BASE_URL}/profile/{test_professional.professional_id}/")
         assert response.status_code == 200
         assert 'account/profile.html' in [template.name for template in response.templates]
         assert f"{test_professional.profile_id.user_id.first_name}'s Profile" in response.content.decode('utf-8')
 
-    def test_show_profile_non_existent_professional(self, client, professional):
-        response = TestProfileViews.get_profile(client=client, entity_id=FAKE_ID, url=BASE_URL)
+    def test_show_profile_non_existent_client(self, client):
+        response = client.get(f"{BASE_URL}/profile/{FAKE_ID}/")
         assert response.status_code == 404
 
-    def test_show_profile_static_files(self, client, professional):
-        test_professional = professional
-        response = TestProfileViews.get_profile(client=client, entity_id=test_professional.professional_id,
-                                                url=BASE_URL)
+    def test_show_profile_static_files(self, client, make_professional):
+        test_professional = make_professional()
+        response = client.get(f"{BASE_URL}/profile/{test_professional.professional_id}/")
         assert 'img/blank_profile.png' in response.content.decode('utf-8')
