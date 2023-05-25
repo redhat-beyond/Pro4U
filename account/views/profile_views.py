c8dab785cb353975efccd74c25ec0c10d8a7e6f2
TalReinfeld <talreinfeld1@gmail.com>
2023-05-25 20:36:09 +0300

    Checking some things
    Signed-off-by: TalReinfeld <talreinfeld1@gmail.com>

--- account/views/profile_views.py
+++ account/views/profile_views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render, get_object_or_404
 def user_profile(request):
     if request.user.is_authenticated:
         return render(request, 'account/profile.html')
-    return render(request, 'homepage.html')
+    return render(request, 'landing/homepage.html')
