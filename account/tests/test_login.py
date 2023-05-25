b56fdcf4423034a65aca9c4a24c50ccbaa824917
Ido Singer <idosinger1998@gmail.com>
2023-05-25 18:08:57 +0300

    I fixed the file account/tests/test_login.py
     1.I added a check that the login.html file is used
     2.Add Login Form validation

--- account/tests/test_login.py
+++ account/tests/test_login.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse
+from account.forms import LoginForm
 
 
 USERNAME = "testusername3"
@@ -11,6 +12,7 @@ class TestLoginView:
     def test_enter_login_page(self, client):
         response = client.get('/login/')
         assert response.status_code == 200
+        assert 'account/login.html' in response.templates[0].name
 
     def test_sign_in_POST_valid(self, client, professional):
         response = client.post('/login/', {
@@ -27,3 +29,12 @@ class TestLoginView:
         })
         assert response.status_code == 200
         assert b'Invalid username or password' in response.content
+
+    def test_login_form_validity(self):
+        data = {
+            'username': USERNAME,
+            'password': PASSWORD,
+        }
+
+        form = LoginForm(data)
+        assert form.is_valid()
