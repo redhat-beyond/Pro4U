c4dac69e04467e3c58a0b976df91728c323c67cd
Ido Singer <idosinger1998@gmail.com>
2023-05-28 18:28:34 +0300

    Change logic and tests of the types of jobs of a professional after login PR merge
    I adjusted the feature design to be part of the system design.
    
    I added in the tests the option to check whether a user is connected or not.
    
    I removed the typeOfJob_confirm_delete.html and when the user clicks on the delete button it does not move him to another page.
    Instead, it simply removes the object and stays on the same page.

--- reservation/test_typeOfJob_addList.py
+++ reservation/test_typeOfJob_addList.py
@@ -15,12 +15,14 @@ def save_type_of_job(make_typeOfJob):
 @pytest.mark.django_db
 class TestTypeOfJob:
     def test_typeOfJob_list(self, client, professional):
-        response = client.get(reverse('typeOfJob', kwargs={'professional': professional.pk}))
+        client.force_login(professional.profile_id.user_id)
+        response = client.get(reverse('typeOfJob'))
         assert response.status_code == 200
         assert 'reservation/typeOfJob_list.html' in response.templates[0].name
 
     def test_create_typeOfJob(self, client, professional, save_type_of_job):
-        response = client.get(reverse('typeOfJob_create', kwargs={'professional': professional.pk}))
+        client.force_login(professional.profile_id.user_id)
+        response = client.get(reverse('typeOfJob_create'))
         assert response.status_code == 200
         assert 'reservation/typeOfJob_form.html' in response.templates[0].name
 
@@ -29,12 +31,13 @@ class TestTypeOfJob:
             'price': 200,
         }
 
-        response = client.post(reverse('typeOfJob_create', kwargs={'professional': professional.pk}), data)
+        response = client.post(reverse('typeOfJob_create'), data)
         assert response.status_code == 302
         assert TypeOfJob.objects.filter(professional_id=professional).count() == 2
-        assert response.url == reverse('typeOfJob', kwargs={'professional': professional.pk})
+        assert response.url == reverse('typeOfJob')
 
     def test_typeOfJob_update(self, client, professional, save_type_of_job):
+        client.force_login(professional.profile_id.user_id)
         url = reverse('typeOfJob_update', args=[save_type_of_job.pk])
         response = client.get(url)
         assert response.status_code == 200
@@ -50,8 +53,10 @@ class TestTypeOfJob:
         assert save_type_of_job.typeOfJob_name == 'Updated Type of Job'
         assert save_type_of_job.price == 150
         assert TypeOfJob.objects.filter(professional_id=professional).count() == 1
+        assert response.url == reverse('typeOfJob')
 
     def test_typeOfJob_delete(self, client, professional, save_type_of_job):
+        client.force_login(professional.profile_id.user_id)
         url = reverse('typeOfJob_delete', args=[save_type_of_job.pk])
         response = client.post(url)
         assert response.status_code == 302
