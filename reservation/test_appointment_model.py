10d950222288761f0dc2b10a2f9338c918dc9c02
Ido Singer <idosinger1998@gmail.com>
2023-06-02 23:00:07 +0300

    Update the tests
    Update conftest.py
    
    Update reservation/test_appointment_model.py
    
    Update reservation/test_my_appointments.py

--- reservation/test_appointment_model.py
+++ reservation/test_appointment_model.py
@@ -9,17 +9,11 @@ END_APPOINTMENT = (current_datetime + timedelta(days=5)).replace(hour=14, minute
 SUMMARY = ""
 
 
-def save_appointment(appointment: Appointment):
-    appointment.professional_id.save()
-    appointment.client_id.save()
-    appointment.typeOfJob_id.save()
-    appointment.save()
-
-
 @pytest.fixture
-def persisted_appointment_pool(make_appointment, demo_client):
+def persisted_appointment_pool(make_appointment, make_client):
+    client = make_client()
     appointment = make_appointment()
-    appointment2 = make_appointment(client_id=demo_client,
+    appointment2 = make_appointment(client_id=client,
                                     start_appointment=(current_datetime + timedelta(days=5)).replace(hour=12, minute=0,
                                                                                                      second=0,
                                                                                                      microsecond=0),
@@ -27,18 +21,21 @@ def persisted_appointment_pool(make_appointment, demo_client):
                                                                                                    second=0,
                                                                                                    microsecond=0),
                                     summary="")
-    save_appointment(appointment)
-    save_appointment(appointment2)
+
     return [appointment, appointment2]
 
 
 @pytest.mark.django_db()
 class TestAppointmentModel:
-    def test_new_appointment(self, make_appointment, professional, demo_client2, make_typeOfJob):
+    def test_new_appointment(self, make_appointment, professional, make_client, make_typeOfJob):
+        client = make_client(username='testusername9',
+                             password='testpassword9',
+                             email='test2@test.com',
+                             phone_number="987654379")
         typeOfJob = make_typeOfJob(professional_id=professional, typeOfJob_name="Gel nail polish", price=90)
-        appointment = make_appointment(typeOfJob_id=typeOfJob)
+        appointment = make_appointment(client_id=client, typeOfJob_id=typeOfJob)
         assert appointment.professional_id == professional
-        assert appointment.client_id == demo_client2
+        assert appointment.client_id == client
         assert appointment.start_appointment == START_APPOINTMENT
         assert appointment.end_appointment == END_APPOINTMENT
         assert appointment.summary == SUMMARY
@@ -73,7 +70,18 @@ class TestAppointmentModel:
                list(Appointment.get_appointments_list_after_current_day(profile_id=professional.professional_id,
                                                                         type_of_user=True))
 
-    def test_get_appointment_list_after_current_day_by_client(self, persisted_appointment_pool, demo_client):
-        assert persisted_appointment_pool[1] == \
-               Appointment.get_appointments_list_after_current_day(profile_id=demo_client.client_id,
+    @pytest.mark.parametrize("appointment", [
+        {
+            "start_appointment": (current_datetime + timedelta(days=5)).replace(hour=12, minute=0, second=0,
+                                                                                microsecond=0),
+            "end_appointment": (current_datetime + timedelta(days=5)).replace(hour=13, minute=0, second=0,
+                                                                              microsecond=0),
+            "summary": ""
+        }
+    ])
+    def test_get_appointment_list_after_current_day_by_client(self, appointment, make_appointment, make_client):
+        client = make_client()
+        appointment["client_id"] = client
+        assert make_appointment(**appointment) == \
+               Appointment.get_appointments_list_after_current_day(profile_id=client.client_id,
                                                                    type_of_user=False)[0]
--- reservation/test_my_appointments.py
+++ reservation/test_my_appointments.py
@@ -16,9 +16,10 @@ class TestAppointmentList:
         assert len(my_appointments) == 1
         assert my_appointments[0] == appointment
 
-    def test_appointment_list_as_client(self, client, demo_client, make_appointment):
-        appointment = make_appointment(client_id=demo_client)  # Pass demo_client argument here
-        client.force_login(demo_client.profile_id.user_id)
+    def test_appointment_list_as_client(self, client, make_client, make_appointment):
+        client1 = make_client()
+        appointment = make_appointment(client_id=client1)  # Pass demo_client argument here
+        client.force_login(client1.profile_id.user_id)
         response = client.get(reverse('my_appointments'))
         assert response.status_code == 200
         assert 'reservation/myAppointments_list.html' in response.templates[0].name
@@ -38,10 +39,11 @@ class TestAppointmentDelete:
         assert response.url == reverse('my_appointments')
         assert Appointment.objects.filter(professional_id=professional).count() == 0
 
-    def test_appointment_delete_by_client(self, demo_client, make_appointment, client):
-        appointment = make_appointment(client_id=demo_client)
-        client.force_login(demo_client.profile_id.user_id)
+    def test_appointment_delete_by_client(self, make_client, make_appointment, client):
+        client1 = make_client()
+        appointment = make_appointment(client_id=client1)
+        client.force_login(client1.profile_id.user_id)
         response = client.post(reverse('appointment_delete', kwargs={'pk': appointment.pk}))
         assert response.status_code == 302
         assert response.url == reverse('my_appointments')
-        assert Appointment.objects.filter(client_id=demo_client).count() == 0
+        assert Appointment.objects.filter(client_id=client1).count() == 0
