48a903012685896b5babb85921931c825f5720f3
Ido Yekutiel <idoyekutiel123@gmail.com>
2023-06-06 16:27:45 +0300

    Updating search feature

--- SearchHistory/test_search_page.py
+++ SearchHistory/test_search_page.py
@@ -1,9 +1,11 @@
 from account.models.professional import Professional
+from reservation.models import TypeOfJob
 from django.urls import reverse
 import pytest
+from account.tests.urls_tests.test_general import PROFILE_URL
 
 CLIENT_ID = 2
-
+PROFESSIONAL_ID = 2
 
 @pytest.mark.django_db
 def test_search_page(client):
@@ -27,8 +29,7 @@ def test_search_by_professional_id(client, make_professional):
     professional = make_professional()
     url = reverse('search history', args=[CLIENT_ID])
     data = {
-            'professional_id': professional.professional_id,
-            'opened': '0',
+            'professional_id': professional.professional_id
         }
     response = client.post(url, data)
 
@@ -42,3 +43,17 @@ def test_search_by_professional_id(client, make_professional):
     assert professionals[0].profile_id.user_id.first_name == professional.profile_id.user_id.first_name
     assert professionals[0].profile_id.user_id.last_name == professional.profile_id.user_id.last_name
     assert professionals[0].profile_id.city == professional.profile_id.city
+
+# @pytest.mark.django_db
+# def test_redirection_to_professional_page(client, make_professional):
+#     professional = make_professional()
+#     typeOfjobs_by_pro = TypeOfJob.get_typeofjobs_by_professional(professional_id=professional.professional_id)
+#     url = reverse('show professional', args=[PROFESSIONAL_ID])
+#     data = {
+#         'professional': professional, 'typeOfjobs_by_pro': typeOfjobs_by_pro
+#     }
+
+#     response = client.post(url, data)
+
+#     assert response.status_code == 200
+#     assert 'account/profile.html' in response.templates[0].name
--- SearchHistory/urls.py
+++ SearchHistory/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
+from account.views.profile_views import show_business_page
 
 
 urlpatterns = [
-    path('search/<int:user_id>/', views.search, name='search history')
-    # path('create_search_history/', views.create_search_history, name='create_search_history')
-
+    path('search/<int:user_id>/', views.search, name='search history'),
+    path('profile/professional/<int:professional_id>/', show_business_page, name='show professional')
 ]
